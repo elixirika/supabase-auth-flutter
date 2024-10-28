a90566a08d6b9504026bf241351b83ee33f7bd9c
elixirika <erikacarinoxx@gmail.com>
2024-10-28 16:02:38 +0100

    Add navigation after login

---
+++ lib/overview.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Overview extends StatefulWidget {
+  const Overview({super.key});
+
+  @override
+  State<Overview> createState() => _OverviewState();
+}
+
+class _OverviewState extends State<Overview> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Overview'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Overview Screen'),
+      ),
+    );
+  }
+}
