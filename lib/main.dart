4ecf847b643fc966a48e3e3a93b01ebc3751f7b8
elixirika <erikacarinoxx@gmail.com>
2024-10-28 16:02:14 +0100

    Add navigation after login

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import './overview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,56 +22,79 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SupaEmailAuth(
-                redirectTo: kIsWeb ? null : 'io.mydomain.myapp://callback',
-                onSignInComplete: (response) {},
-                onSignUpComplete: (response) {},
-                metadataFields: [
-                  MetaDataField(
-                    prefixIcon: const Icon(Icons.person),
-                    label: 'Username',
-                    key: 'username',
-                    validator: (val) {
-                      if (val == null || val.isEmpty) {
-                        return 'Please enter something';
-                      }
-                      return null;
-                    },
-                  ),
-                ],
-              ),
-              // SupaMagicAuth(
-              //   redirectUrl: kIsWeb ? null : 'io.mydomain.myapp://callback',
-              //   onSuccess: (Session response) {},
-              //   onError: (error) {},
-              // ),
-              // SupaResetPassword(
-              //   accessToken: supabase.auth.currentSession?.accessToken,
-              //   onSuccess: (UserResponse response) {},
-              //   onError: (error) {},
-              // ),
-              // SupaPhoneAuth(
-              //   authAction: SupaAuthAction.signUp,
-              //   onSuccess: (AuthResponse response) {},
-              // ),
-              SupaSocialsAuth(
-                socialProviders: [
-                  OAuthProvider.apple,
-                  OAuthProvider.google,
-                ],
-                colored: true,
-                redirectUrl: kIsWeb ? null : 'io.mydomain.myapp://callback',
-                onSuccess: (Session response) {},
-                onError: (error) {},
-              )
-            ],
-          ),
+      home: AuthChecker(), // Check if user is already signed in
+    );
+  }
+}
+
+class AuthChecker extends StatefulWidget {
+  const AuthChecker({super.key});
+
+  @override
+  AuthCheckerState createState() => AuthCheckerState();
+}
+
+class AuthCheckerState extends State<AuthChecker> {
+  @override
+  void initState() {
+    super.initState();
+    _checkSession();
+  }
+
+  Future<void> _checkSession() async {
+    final session = Supabase.instance.client.auth.currentSession;
+    if (kDebugMode) {
+      print(session);
+    }
+    if (session != null) {
+      // schedule the navigation to happen after the build is complete
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const Overview()),
+        );
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Builder(
+              builder: (BuildContext context) {
+                return SupaEmailAuth(
+                  redirectTo: kIsWeb ? null : 'io.mydomain.myapp://callback',
+                  onSignInComplete: (response) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => const Overview()),
+                      );
+                    });
+                  },
+                  onSignUpComplete: (response) {},
+                  metadataFields: [
+                    MetaDataField(
+                      prefixIcon: const Icon(Icons.person),
+                      label: 'Username',
+                      key: 'username',
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return 'Please enter something';
+                        }
+                        return null;
+                      },
+                    ),
+                  ],
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
