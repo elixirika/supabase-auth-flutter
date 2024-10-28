506327b39d566efa50d18c13c5bb22997fcc5c72
elixirika <erikacarinoxx@gmail.com>
2024-10-28 14:34:03 +0100

    Setup env and minimum macOS target

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
+  await Supabase.initialize(
+    url: dotenv.get('SUPABASE_URL', fallback: ''),
+    anonKey: dotenv.get('SUPABASE_ANON_KEY', fallback: ''),
+  );
+
   runApp(const MainApp());
 }
 
@@ -9,10 +20,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SupaEmailAuth(
+                redirectTo: kIsWeb ? null : 'io.mydomain.myapp://callback',
+                onSignInComplete: (response) {},
+                onSignUpComplete: (response) {},
+                metadataFields: [
+                  MetaDataField(
+                    prefixIcon: const Icon(Icons.person),
+                    label: 'Username',
+                    key: 'username',
+                    validator: (val) {
+                      if (val == null || val.isEmpty) {
+                        return 'Please enter something';
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
+              // SupaMagicAuth(
+              //   redirectUrl: kIsWeb ? null : 'io.mydomain.myapp://callback',
+              //   onSuccess: (Session response) {},
+              //   onError: (error) {},
+              // ),
+              // SupaResetPassword(
+              //   accessToken: supabase.auth.currentSession?.accessToken,
+              //   onSuccess: (UserResponse response) {},
+              //   onError: (error) {},
+              // ),
+              // SupaPhoneAuth(
+              //   authAction: SupaAuthAction.signUp,
+              //   onSuccess: (AuthResponse response) {},
+              // ),
+              SupaSocialsAuth(
+                socialProviders: [
+                  OAuthProvider.apple,
+                  OAuthProvider.google,
+                ],
+                colored: true,
+                redirectUrl: kIsWeb ? null : 'io.mydomain.myapp://callback',
+                onSuccess: (Session response) {},
+                onError: (error) {},
+              )
+            ],
+          ),
         ),
       ),
     );
