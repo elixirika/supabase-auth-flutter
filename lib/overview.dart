d3566eb516a8a7359626a272d3ca87c092fce1a6
elixirika <erikacarinoxx@gmail.com>
2024-10-28 16:03:07 +0100

    Add Logout button

--- lib/overview.dart
+++ lib/overview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import './main.dart';
 
 class Overview extends StatefulWidget {
   const Overview({super.key});
@@ -13,6 +15,20 @@ class _OverviewState extends State<Overview> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Overview'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: () async {
+              await Supabase.instance.client.auth.signOut();
+              if (context.mounted) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AuthChecker()),
+                );
+              }
+            },
+          ),
+        ],
       ),
       body: Center(
         child: Text('Welcome to the Overview Screen'),
